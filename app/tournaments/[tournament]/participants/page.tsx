74beb454b008e38a10ce800fd616ffaeee5b823d
yaowan233 <572473053@qq.com>
2024-03-08 17:42:19 +0800

    更新

--- app/tournaments/[tournament]/participants/page.tsx
+++ app/tournaments/[tournament]/participants/page.tsx
@@ -3,14 +3,28 @@ import {ParticipantsComp} from "@/components/participants_comp";
 import {siteConfig} from "@/config/site";
 
 export default async function ParticipantsPage({params}: { params: { tournament: string } }) {
-    const players = await getPlayers(params.tournament)
+    const tournament_players = await getPlayers(params.tournament)
     return (
-        <ParticipantsComp players={players}/>
+        <ParticipantsComp tournament_players={tournament_players}/>
     )
 }
 
-async function getPlayers(tournament_name: string): Promise<Player[]> {
+async function getPlayers(tournament_name: string): Promise<TournamentPlayers> {
     const res = await fetch(siteConfig.backend_url + '/api/players?tournament_name=' + tournament_name,
         { next: { revalidate: 60 }})
     return await res.json()
+}
+
+
+export interface TournamentPlayers {
+    groups?: Team[];
+    players?: Player[];
+}
+type Team = {
+    tournament_name: string
+    name: string
+    captains: number[]
+    members: number[]
+    icon_url?: string
+    is_verified: boolean
 }
--- components/participants_comp.tsx
+++ components/participants_comp.tsx
@@ -2,28 +2,80 @@
 
 import {Tab, Tabs} from "@nextui-org/tabs";
 import {Card, CardBody} from "@nextui-org/card";
-import {Player, UserInfo} from "@/components/user_info";
+import {UserInfo} from "@/components/user_info";
+import {TournamentPlayers} from "@/app/tournaments/[tournament]/participants/page";
+import {Image} from "@nextui-org/image";
+import {Link} from "@nextui-org/link";
+import {Tooltip} from "@nextui-org/tooltip";
 
-export const ParticipantsComp = ({players}: { players: Player[] }) => {
+export const ParticipantsComp = ({tournament_players}: { tournament_players: TournamentPlayers }) => {
+    const players = tournament_players.players
+    const teams = tournament_players.groups
     return (
         <Tabs aria-label="Options">
             <Tab key="solo" title="报名人员">
                 <Card>
                     <CardBody>
                         <div className={"grid md:grid-cols-4 sm:grid-cols-3 gap-3"}>
-                            {players.sort((player1, player2) => parseFloat(player2.pp) - parseFloat(player1.pp)).map((user, num) => (
+                            {players?.sort((player1, player2) => parseFloat(player2.pp) - parseFloat(player1.pp)).map((user, num) => (
                                 <UserInfo key={num} user={user}/>
                             ))}
                         </div>
                     </CardBody>
                 </Card>
             </Tab>
-            {/*<Tab key="teams" title="队伍">*/}
-            {/*    <Card>*/}
-            {/*        <CardBody>*/}
-            {/*        </CardBody>*/}
-            {/*    </Card>*/}
-            {/*</Tab>*/}
+            <Tab key="teams" title="队伍">
+                <Card>
+                    <CardBody>
+                        <div className={"grid md:grid-cols-4 sm:grid-cols-3 gap-3"}>
+                            {teams?.map((team, num) => (
+                                <div key={num} className={"flex flex-col border-2 p-2"}>
+                                    <div className="flex flex-row mb-2">
+                                        <Image alt="icon" height={64} width={64} src={team.icon_url? team.icon_url: "https://a.ppy.sh"}/>
+                                        <p className={"text-foreground text-center w-full text-2xl self-center line-clamp-1"}>
+                                            {team.name}
+                                        </p>
+                                    </div>
+                                    {team.captains.map((user, num) => {
+                                        const player = players?.find(player => player.uid === user)
+                                        if (player) {
+                                            return (
+                                                <div className="flex flex-row w-full justify-between" key={num}>
+                                                    <Tooltip content="队长" placement="right">
+                                                        <Link isExternal color="foreground" className="font-bold" href={`https://osu.ppy.sh/u/${player.uid}`}>
+                                                            {player.name}
+                                                        </Link>
+                                                    </Tooltip>
+                                                    <div>
+                                                        #{player.rank}
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                        return null
+                                    })}
+                                    {team.members.map((user, num) => {
+                                        const player = players?.find(player => player.uid === user)
+                                        if (player) {
+                                            return (
+                                                <div className="flex flex-row w-full justify-between" key={num}>
+                                                    <Link isExternal color="foreground" href={`https://osu.ppy.sh/u/${player.uid}`}>
+                                                        {player.name}
+                                                    </Link>
+                                                    <div>
+                                                        #{player.rank}
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                        return null
+                                    })}
+                                </div>
+                            ))}
+                        </div>
+                    </CardBody>
+                </Card>
+            </Tab>
         </Tabs>
     )
 }
