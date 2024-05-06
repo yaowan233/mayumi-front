fc26e51b0218f5b978d82581a894ce196999a260
yaowan233 <572473053@qq.com>
2024-05-06 15:00:48 +0800

    修复bug

--- app/tournaments/[tournament]/home/page.tsx
+++ app/tournaments/[tournament]/home/page.tsx
@@ -4,12 +4,15 @@ import {siteConfig} from "@/config/site";
 
 export default async function TournamentHomePage({params}: { params: { tournament: string } }) {
     const info = await getTournamentInfo(params.tournament)
+    if (!info) {
+        return <div className='text-center text-2xl font-bold'>没有这种比赛喵~</div>
+    }
     return (
         <HomePage tournament_info={info}/>
     )
 }
 
-async function getTournamentInfo(tournament_name: string): Promise<TournamentInfo> {
+async function getTournamentInfo(tournament_name: string): Promise<TournamentInfo | null> {
     const res = await fetch(siteConfig.backend_url + '/api/tournament-info?tournament_name=' + tournament_name,
         { next: { revalidate: 60 }})
     return await res.json()
