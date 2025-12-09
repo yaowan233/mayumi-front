24c66197b12785c94eae2e3cdeb9b95a9dceb241
yaowan233 <572473053@qq.com>
2025-12-09 17:21:47 +0800

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,11 +57,11 @@ export default function RootLayout({
                         <Link
                             isExternal
                             className="flex items-center gap-1 text-current"
-                            href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                            title="nextui.org homepage"
+                            href="https://heroui.com?utm_source=next-app-template"
+                            title="heroui.com homepage"
                         >
                             <span className="text-default-600">Powered by</span>
-                            <p className="text-primary">NextUI</p>
+                            <p className="text-primary">HeroUI</p>
                         </Link>
                     </footer>
                 </div>
